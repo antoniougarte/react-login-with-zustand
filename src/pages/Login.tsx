32661ea5f4ae410533c8f219c6fa9f1457698eb7
Paulo <paulo.ugarte@alarcongroup.us>
2024-01-23 22:12:34 -0500

    fix tailwind.comfig and sweet alert element

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import { useUserStore } from "../stores/user/user.store";
 import { Navigate, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export const Login = () => {
   const navigation = useNavigate()
@@ -24,7 +25,16 @@ export const Login = () => {
       navigation("/")
     }
   };
-  if (localStorage.getItem("tokenAuth")) return <Navigate to="/" />
+  if (localStorage.getItem("tokenAuth")){
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Session successfully logged in",
+      showConfirmButton: false,
+      timer: 1500
+    });
+    return <Navigate to="/" />
+  }
   return (
     <>
       <section className="bg-gray-50 dark:bg-gray-900">
@@ -47,7 +57,7 @@ export const Login = () => {
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                             <input value={password} onChange={handlePasswordChange} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
-                        <button type="submit" className="w-full dark:text-white text-black ">Sign in</button>
+                        <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                     </form>
                 </div>
             </div>
